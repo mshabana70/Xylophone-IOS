360d161f7a55805be6af8fc2c023b14345f6e162
Mahmoud Shabana <mshabana5656@gmail.com>
2020-06-04 15:49:27 -0400

    Change button opacity

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -21,6 +21,17 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         
         playSound(soundName: sender.currentTitle!)
+        
+        //Simulate a button being pressed visually with these steps:
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(soundName: String) {
