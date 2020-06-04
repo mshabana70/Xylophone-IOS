639f9a1049840c7a963e7cb9617bb405e59ef214
Mahmoud Shabana <mshabana5656@gmail.com>
2020-06-04 15:36:38 -0400

    Finished app with use of functions with inputs

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -3,19 +3,33 @@
 //  Xylophone
 //
 //  Created by Angela Yu on 28/06/2019.
+//  Modified by Mahmoud Shabana on 6/02/202
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
+
 
